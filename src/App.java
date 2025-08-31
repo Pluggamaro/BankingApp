7cc9ca6aeafc87233d9b10df5ba5f57ba0689b95
Bongani X <velanibzuma@gmail.com>
2025-08-31 23:32:52 +0200

    -methods implemented, functional

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -12,7 +13,49 @@ public class App {
 
             while(isRunning){
                 printMenu();
+
+                System.out.print("Make choice by number: ");
+
+                try {
+                    int choice = scanner.nextInt();
+
+                    switch (choice){
+                        case 1:
+                            System.out.print("Enter amount to withdraw: ");
+                            int withdrawalAmount = scanner.nextInt();
+                            userAccount.withdraw(withdrawalAmount);
+                            break;
+                        case 2:
+                            System.out.print("Enter amount to deposit: ");
+                            int depositAmount = scanner.nextInt();
+                            userAccount.deposit(depositAmount);
+                            break;
+                        case 3:
+                            userAccount.checkBalance();
+                            break;
+                        case 4:
+                            isRunning = false;
+                            System.out.println("Ejecting Card...Thank you!");
+                            break;
+                            
+                        default:
+                            System.out.println("Invalid. Enter an option 1-4");    
+                    }
+                }catch(InputMismatchException e){
+                    System.out.println("Invalid. Enter a NUMBER!");
+                    scanner.next();
+                }
             }
         }
     }
+
+    private static void printMenu() {
+        System.out.println("========SIMPLE BANKING APP========");
+        System.out.println("         1. Withdraw");
+        System.out.println("         2. Deposit ");
+        System.out.println("         3. Check Balance");
+        System.out.println("         4. Give Back Card (EXIT)");
+        System.out.println("----------------------------------");
+    }
+
 }
