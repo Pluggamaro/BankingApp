3affaafd3aceb81b3b02e1338231b075d8151113
Bongani X <velanibzuma@gmail.com>
2025-08-31 23:11:38 +0200

    -Implementing main app using try-with-resources for easier resource management

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //creating mock account
+        BankAccount userAccount = new BankAccount("Bongani Zuma",14000.00);
+
+        //using try-with resources to auto-close scanner resources
+
+        try(Scanner scanner = new Scanner(System.in)){
+            boolean isRunning = true;
+
+            while(isRunning){
+                printMenu();
+            }
+        }
     }
 }
